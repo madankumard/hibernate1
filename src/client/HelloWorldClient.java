1b3a1c1f9b10bc868e15579d834d6a60db9bcbf2
MadanKumar <madan739@gmail.com>
2020-07-19 16:29:59 +0530

    Added Person and Address Entity - Embeddable

--- src/client/HelloWorldClient.java
+++ src/client/HelloWorldClient.java
@@ -1,13 +1,33 @@
 package client;
 
+import entity.Address;
 import entity.Message;
+import entity.Person;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import util.HibernateUtil;
 
 public class HelloWorldClient {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        Transaction txn = session.getTransaction();
+
+        try{
+            txn.begin();
+            Address homeAddress = new Address("200 E Main St", "Seattle", "345345");
+            Address billingAddress = new Address("100 E Main St", "Seattle2", "245345");
+            Person person = new Person("ruby", homeAddress, billingAddress);
+            session.save(person);
+            txn.commit();
+        }catch (Exception e){
+            if(txn != null) txn.rollback();
+            e.printStackTrace();
+        }finally {
+            if(session!=null) session.close();
+        }
+
+
+
 
         //Save
         //Message message = new Message("Hello World");
@@ -18,14 +38,14 @@ public class HelloWorldClient {
         //System.out.println(message);
 
         //Update
-        Message message = session.get(Message.class, 3L);
-        message.setText("Automatic Dirty Check 2");
+        //Message message = session.get(Message.class, 3L);
+        //message.setText("Automatic Dirty Check 2");
 
         //Delete
-        message = session.get(Message.class, 4L);
-        session.delete(message);
+        //message = session.get(Message.class, 4L);
+        //session.delete(message);
 
-        session.getTransaction().commit();
-        session.close();
+        //session.getTransaction().commit();
+        //session.close();
     }
 }
---
+++ src/entity/Person.java
@@ -0,0 +1,39 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "home_zipcode"))
+    })
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "billing_street")),
+            @AttributeOverride(name = "city", column = @Column(name = "billing_city")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "billing_zipcode"))
+    })
+    private Address billingAddress;
+
+    public Person() {
+    }
+
+    public Person(String name, Address homeAddress, Address billingAddress) {
+        this.name = name;
+        this.homeAddress = homeAddress;
+        this.billingAddress = billingAddress;
+    }
+}
